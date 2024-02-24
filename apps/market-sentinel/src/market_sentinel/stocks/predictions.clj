c3c94ee42eb235fde431ed923e04c72a7836fb5f
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-02-24 19:39:16 +0700

    fix(market-sentinel): fix prediction calculator v0.1

--- apps/market-sentinel/src/market_sentinel/stocks/predictions.clj
+++ apps/market-sentinel/src/market_sentinel/stocks/predictions.clj
@@ -31,19 +31,17 @@
      :pe-healthy-target (*
                          pe-snp-500-avg
                          (- 1 (/ pe-safety-margin-pct 100)))
-     :growth-6mo-weight 1
      :growth-1y-weight  2
-     :growth-3y-weight  3
      :growth-5y-weight  5}))
 
-(defn get-1y-growth-expectation-pct [growth-1y-pct  growth-5y-pct]
-  (let [growth-1y-weight (:growth-1y-weight stock-params)
-        growth-5y-weight (:growth-5y-weight stock-params)
-        term-1y          (* (+ 1 (/ growth-1y-pct 100)) growth-1y-weight)
-        avg-growth-5y    (Math/pow (+ 1 (/ growth-5y-pct 100)) (/ 1 5))
-        term-5y          (* avg-growth-5y growth-5y-weight)
-        numerator        (+ term-1y term-5y)
-        denominator      (+ growth-1y-weight growth-5y-weight)]
+(defn predict-1y-growth-pct [growth-1y-pct  growth-5y-pct]
+  (let [growth-1y-weight  (:growth-1y-weight stock-params)
+        growth-5y-weight  (:growth-5y-weight stock-params)
+        growth-1y-contrib (* (+ 1 (/ growth-1y-pct 100)) growth-1y-weight)
+        avg-growth-5y     (Math/pow (+ 1 (/ growth-5y-pct 100)) (/ 1 5))
+        growth-5y-contrib (* avg-growth-5y growth-5y-weight)
+        numerator         (+ growth-1y-contrib growth-5y-contrib)
+        denominator       (+ growth-1y-weight growth-5y-weight)]
     (try (* 100 (- (/ numerator denominator) 1))
          (catch Exception _e nil))))
 
@@ -51,24 +49,24 @@
   "predict-ticker will predict the future of a given ticker"
   [ticker-info]
   (let
-   [trailing-pe               (get-in ticker-info [:fundamentals :trailing-pe])
-    forward-pe                (get-in ticker-info [:fundamentals :forward-pe])
-    growth-1y-expectation-pct (get-1y-growth-expectation-pct
-                               (:growth-1y-pct (:growth ticker-info))
-                               (:growth-5y-pct (:growth ticker-info)))
-    pe-healthy-target         (:pe-healthy-target stock-params)
-    predict-profit            (fn [pe]
-                                (try
-                                  (- (*
-                                      (+ 100 growth-1y-expectation-pct)
-                                      (/ pe-healthy-target pe))
-                                     100)
-                                  (catch Exception _e nil)))]
+   [trailing-pe              (get-in ticker-info [:fundamentals :trailing-pe])
+    forward-pe               (get-in ticker-info [:fundamentals :forward-pe])
+    pe-healthy-target        (:pe-healthy-target stock-params)
+    growth-1y-prediction-pct (predict-1y-growth-pct
+                              (:growth-1y-pct (:growth ticker-info))
+                              (:growth-5y-pct (:growth ticker-info)))
+    predict-profit           (fn [pe]
+                               (try
+                                 (- (*
+                                     (+ 100 growth-1y-prediction-pct)
+                                     (/ pe-healthy-target pe))
+                                    100)
+                                 (catch Exception _e nil)))]
 
     {:stock-ticker-code                (:code (:ticker ticker-info))
      :profit_prediction-by-trailing-pe (predict-profit trailing-pe)
      :profit_prediction-by-forward-pe  (predict-profit forward-pe)
-     :growth-1y-expectation            growth-1y-expectation-pct}))
+     :growth-1y-prediction-pct         growth-1y-prediction-pct}))
 
 (defn store-tickers-predictions!
   "store-tickers-predictions! will store the predictions of the tickers in the database"
@@ -79,11 +77,11 @@
    (sql/format
     {:insert-into   :market-sentinel.stock_predictions
      :values        (map
-                     (fn [{:keys [stock-ticker-code profit_prediction-by-trailing-pe profit_prediction-by-forward-pe growth-1y-expectation]}]
+                     (fn [{:keys [stock-ticker-code profit_prediction-by-trailing-pe profit_prediction-by-forward-pe growth-1y-prediction-pct]}]
                        {:stock_ticker_code                stock-ticker-code
                         :profit_prediction_by_trailing_pe profit_prediction-by-trailing-pe
                         :profit_prediction_by_forward_pe  profit_prediction-by-forward-pe
-                        :growth_1y_prediction             growth-1y-expectation})
+                        :growth_1y_prediction             growth-1y-prediction-pct})
                      ticker-predictions)
      :on-conflict   [:stock_ticker_code]
      :do-update-set {:fields [:stock_ticker_code :profit_prediction_by_trailing_pe :profit_prediction_by_forward_pe :growth_1y_prediction :updated_at]}})))
