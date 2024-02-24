107fe48a2946430bb5e1a0eedb228b33479577a7
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-02-24 12:34:45 +0700

    feat(market-sentinel): add prediction calculator v0

--- apps/market-sentinel/src/market_sentinel/stocks/core.clj
+++ apps/market-sentinel/src/market_sentinel/stocks/core.clj
@@ -1,48 +1,13 @@
 (ns market-sentinel.stocks.core
-  (:require [clojure.math.numeric-tower :refer [expt]]
-            [market-sentinel.stocks.eod :refer [fetch-tickers-eods
+  (:require [market-sentinel.stocks.eod :refer [fetch-tickers-eods
                                                 store-tickers-eods!]]
             [market-sentinel.stocks.fundamentals
-             :refer [fetch-ticker-fundamentals clean-ticker-fundamental
+             :refer [clean-ticker-fundamental fetch-ticker-fundamentals
                      store-tickers-fundamentals!]]
+            [market-sentinel.stocks.predictions :refer [extract-ticker-data-for-predictions
+                                                        predict-ticker
+                                                        store-tickers-predictions!]]
             [market-sentinel.stocks.tickers :refer [extract-all-stock-tickers]]))
-
-(def stock-params (let [pe-nasdaq-avg            25.03
-                        pe-snp-500-avg           23.27
-                        pe-safety-margin-percent 25]
-                    {:nasdaq-avg-pe     pe-nasdaq-avg
-                     :snp-500-avg-pe    pe-snp-500-avg
-                     :pe-healthy-target (* pe-snp-500-avg (- 1 (/ pe-safety-margin-percent 100)))
-                     :growth-6mo-weight 1
-                     :growth-1y-weight  2
-                     :growth-3y-weight  3
-                     :growth-5y-weight  4}))
-
-(defn get-1y-growth-expectation [growth-1y-percent growth-5y-percent]
-  (/ (+ (* (/ growth-1y-percent 100) (:growth-1y-weight stock-params))
-        (*
-         (expt (/ growth-5y-percent 100) (/ 1 5))
-         (:growth-5y-weight stock-params)))
-     (+ (:growth-1y-weight stock-params) (:growth-5y-weight stock-params))))
-
-(defn analyze-ticker [fundamental-data]
-  (let [{:keys [reference]}              fundamental-data
-        {:keys [trailing-pe
-                forward-pe]} reference
-        ;; TODO: get it from the API
-        growth-1y-percent                17.06
-        growth-5y-percent                190.5
-        growth-1y-expectation-percent    (get-1y-growth-expectation growth-1y-percent growth-5y-percent)]
-    (into
-     {}
-     [fundamental-data
-      {:analysis {:growth-target-by-trailing-pe (-
-                                                 (+ 1 growth-1y-expectation-percent)
-                                                 (/ trailing-pe (:pe-healthy-target stock-params)))
-                  :growth-target-by-forward-pe  (-
-                                                 (+ 1 growth-1y-expectation-percent)
-                                                 (/ forward-pe (:pe-healthy-target stock-params)))
-                  :growth-1y-expectation        growth-1y-expectation-percent}}])))
 
 (comment
   ;; get fundamental data
@@ -56,4 +21,11 @@
   (->> (extract-all-stock-tickers)
        (map (fn [m]  (:code  m)))
        (fetch-tickers-eods (* 365 5))
-       (map (fn [[ticker eods]] (store-tickers-eods! {ticker eods})))))
+       (map (fn [[ticker eods]] (store-tickers-eods! {ticker eods}))))
+  ;; generate prediction data
+  (->> (extract-all-stock-tickers)
+       (map (fn [ticker]
+              (->> ticker
+                   extract-ticker-data-for-predictions
+                   predict-ticker)))
+       (store-tickers-predictions!)))
--- apps/market-sentinel/src/market_sentinel/stocks/predictions.clj
+++ apps/market-sentinel/src/market_sentinel/stocks/predictions.clj
@@ -1,7 +1,11 @@
 (ns market-sentinel.stocks.predictions
-  (:require
-   [market-sentinel.stocks.eod :refer [extract-eod-summary-for-ticker]]
-   [market-sentinel.stocks.fundamentals :refer [extract-ticker-consensus extract-ticker-fundamentals]]))
+  (:require [honey.sql :as sql]
+            [market-sentinel.infra.db.core :refer [ds]]
+            [market-sentinel.stocks.eod :refer [extract-eod-summary-for-ticker]]
+            [market-sentinel.stocks.fundamentals :refer [extract-ticker-consensus
+                                                         extract-ticker-fundamentals]]
+            [market-sentinel.stocks.tickers :refer [extract-all-stock-tickers]]
+            [next.jdbc :as jdbc]))
 
 (defn extract-ticker-data-for-predictions
   "extract-ticker-data-for-highlights will extract necessary data for ticker highlights"
@@ -18,7 +22,86 @@
      :consensus    consensus
      :growth       growth}))
 
+(def stock-params
+  (let [pe-nasdaq-avg            25.03
+        pe-snp-500-avg           23.27
+        pe-safety-margin-percent 25]
+    {:nasdaq-avg-pe     pe-nasdaq-avg
+     :snp-500-avg-pe    pe-snp-500-avg
+     :pe-healthy-target (*
+                         pe-snp-500-avg
+                         (- 1 (/ pe-safety-margin-percent 100)))
+     :growth-6mo-weight 1
+     :growth-1y-weight  2
+     :growth-3y-weight  3
+     :growth-5y-weight  5}))
+
+(defn get-1y-growth-expectation [growth-1y-percent  growth-5y-percent]
+  (let [growth-1y-weight (:growth-1y-weight stock-params)
+        growth-5y-weight (:growth-5y-weight stock-params)
+        term-1y          (* (+ 1 (/ growth-1y-percent 100)) growth-1y-weight)
+        avg-growth-5y    (Math/pow (+ 1 (/ growth-5y-percent 100)) (/ 1 5))
+        term-5y          (* avg-growth-5y growth-5y-weight)
+        numerator        (+ term-1y term-5y)
+        denominator      (+ growth-1y-weight growth-5y-weight)]
+    (* 100 (- (/ numerator denominator) 1))))
+(get-1y-growth-expectation 109.64  5244.17)
+
+(defn predict-ticker
+  "predict-ticker will predict the future of a given ticker"
+  [ticker-info]
+  (let
+   [trailing-pe                   (get-in ticker-info [:fundamentals :trailing-pe])
+    forward-pe                    (get-in ticker-info [:fundamentals :forward-pe])
+    growth-1y-expectation-percent (get-1y-growth-expectation
+                                   (:growth-1y-percent (:growth ticker-info))
+                                   (:growth-5y-percent (:growth ticker-info)))
+    pe-healthy-target             (:pe-healthy-target stock-params)]
+
+    {:stock-ticker-code            (:code (:ticker ticker-info))
+     :growth-target-by-trailing-pe (try (*
+                                         (+ 1 growth-1y-expectation-percent)
+                                         (/ pe-healthy-target trailing-pe))
+                                        (catch Exception _e nil))
+     :growth-target-by-forward-pe  (try (*
+                                         (+ 1 growth-1y-expectation-percent)
+                                         (/ pe-healthy-target forward-pe))
+                                        (catch Exception _e nil))
+     :growth-1y-expectation        growth-1y-expectation-percent}))
+
+(defn store-tickers-predictions!
+  "store-tickers-predictions! will store the predictions of the tickers in the database"
+  [ticker-predictions]
+
+  (jdbc/execute!
+   ds
+   (sql/format
+    {:insert-into   :market-sentinel.stock_highlights
+     :values        (map
+                     (fn [{:keys [stock-ticker-code growth-target-by-trailing-pe growth-target-by-forward-pe]}]
+                       {:stock_ticker_code         stock-ticker-code
+                        :wkf_rating_by_trailing_pe growth-target-by-trailing-pe
+                        :wkf_rating_by_forward_pe  growth-target-by-forward-pe})
+                     ticker-predictions)
+     :on-conflict   [:stock_ticker_code]
+     :do-update-set {:fields [:stock_ticker_code :wkf_rating_by_trailing_pe :wkf_rating_by_forward_pe :updated_at]}})))
+
+(def a (->> (extract-all-stock-tickers)
+            (map (fn [ticker]
+                   (->> ticker
+                        extract-ticker-data-for-predictions
+                        predict-ticker)))
+            doall))
+(store-tickers-predictions! a)
+
 (comment
-  (extract-ticker-data-for-predictions
-   {:code     "CELH"
-    :exchange "NASDAQ"}))
+  (->> {:code     "NVDA"
+        :exchange "NASDAQ"}
+       extract-ticker-data-for-predictions
+       predict-ticker)
+  (->> (extract-all-stock-tickers)
+       (map (fn [ticker]
+              (->> ticker
+                   extract-ticker-data-for-predictions
+                   predict-ticker)))
+       (store-tickers-predictions!)))
