1b8e1ad5e3779e3787b6869aa43dee67514f8fc3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-02-25 07:55:57 +0700

    chore(market-sentinel): code refactor and add tests

--- apps/market-sentinel/src/market_sentinel/stocks/config.clj
+++ apps/market-sentinel/src/market_sentinel/stocks/config.clj
@@ -1,13 +1,14 @@
 (ns market-sentinel.stocks.config)
 
 (defn calculate-healthy-pe
-  "calculate-healthy-pe will calculate healthy PE for a given PE and safety margin percentage. It accept PE and safety margin percentage as input and return healthy PE as output. It will return nil when either PE or safety margin percentage is nil."
+  "`calculate-healthy-pe` will calculate healthy PE for a given PE and safety margin percentage. It accept PE and safety margin percentage as input and return healthy PE as output. It will return `nil` when either PE or safety margin percentage is `nil`."
   [pe safety-margin-pct]
   (if (or (nil? pe) (nil? safety-margin-pct))
     nil
     (* pe (- 1 (/ safety-margin-pct 100)))))
 
 (def stock-market-config
+  "`stock-market-config` contains the configuration for the stock market. It can be used to predict the future of a given ticker."
   (let [pe-nasdaq-avg        25.03
         pe-snp-500-avg       23.27
         pe-safety-margin-pct 25]
